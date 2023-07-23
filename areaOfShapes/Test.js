1caa98a09e86759da8011e4544b9ca0b74a5f497
ritik <ritiksharmaformal>
2023-07-23 14:50:51 +0530

    done

---
+++ areaOfShapes/Test.js
@@ -0,0 +1,10 @@
+const area=require('./area.js');
+
+area.Rectangle(5,6);
+area.Triangle(4,5);
+area.circle(6);
+
+
+
+
+
---
+++ areaOfShapes/area.js
@@ -0,0 +1,21 @@
+function circle(radius ){
+    console.log(2*2.14*Number(radius));
+    
+    
+}
+
+
+function Triangle(length,breadth ){
+    console.log(0.5*Number(length)*Number(breadth));
+    
+    
+}
+
+
+function Rectangle(length,breadth ){
+    console.log(Number(length)*Number(breadth));
+    
+    
+}
+
+module.exports={circle,Rectangle,Triangle};
